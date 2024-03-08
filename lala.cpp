2454e6bbf3efac8b0b23bbaa6c4561057283ea5f
zhangxheng <445139574@qq.com>
2024-03-09 00:25:43 +0800

    第一版

---
+++ lala.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "Hello World" << endl;
+  cout << "Hello World" << endl;
+  return 0; // return 0 is used to terminate the main function and return 0 indicates that the program ended successfully.
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  printf("Hello World\n");
+  printf("Hello World\n");
+  printf("Hello World\n");
+  // printf("Hello Git\n");
+  return 0;
+}
